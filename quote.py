3a0cfcdeeb76f79ef0445dc28197438cf56c07cc
madprops <manuelchaves@gmail.com>
2016-01-31 15:50:57 -0600

    'vmat'

--- quote.py
+++ quote.py
@@ -17,14 +17,9 @@ def quote(bot, trigger, found_match=None):
 
 	arguments = trigger[3:].strip()
 
-	if trigger.sender == '#neritic-net':
-		fname = '/home/willie/neritic.log'
 
-	elif trigger.sender == '#podricing':
-		fname = '/home/willie/podricing.log'
+	fname = '/home/willie/' + trigger.sender + '.log'
 
-	else:
-		return
 
 	if arguments[0:7] == 'search ':
 		string = arguments[7:]
@@ -32,7 +27,7 @@ def quote(bot, trigger, found_match=None):
 		with open(fname, 'r') as f:
 			log = f.read()
 
-		p = re.compile(ur"^\w+ \d+ \d+:\d+:\d+ (<[^>]+>\t.*\b" + string + ur"\b.*)", re.MULTILINE | re.IGNORECASE)
+		p = re.compile(ur"^\w+ \d+ \d+:\d+:\d+ (<(?!madbot|flanfly|SuikaIbuki|Wobbuffet|Internets|kindbot|ChanStat)[^>]+>\t(?![.!~,]).*\b" + string + ur"\b.*)", re.MULTILINE | re.IGNORECASE)
 
 		try:
 
@@ -44,14 +39,32 @@ def quote(bot, trigger, found_match=None):
 
 			bot.say('no quote found' + dots() + ' you searched for ' + string)
 
+	elif arguments[0:6] == 'count ':
+		string = arguments[6:]
+
+		with open(fname, 'r') as f:
+			log = f.read()
+
+		p = re.compile(ur"^\w+ \d+ \d+:\d+:\d+ (<(?!madbot|flanfly|SuikaIbuki|Wobbuffet|Internets|kindbot|ChanStat)[^>]+>\t(?![.!~,]).*\b" + string + ur"\b.*)", re.MULTILINE | re.IGNORECASE)
+
+		try:
+
+			results = re.findall(p, log)
+
+			bot.say("'" + string + "' has been said " + str(len(results)) + " times")
+
+		except:
+
+			bot.say('no quote found' + dots() + ' you searched for ' + string)
+
 	else:
 		with open(fname, 'r') as f:
 			log = f.read()
 
 		if arguments != '':
-			p = re.compile(ur"^\w+ \d+ \d+:\d+:\d+ (<" + arguments + ur">\t.*)", re.MULTILINE | re.IGNORECASE)
+			p = re.compile(ur"^\w+ \d+ \d+:\d+:\d+ (<" + arguments + ur">\t[^.!~,].*)", re.MULTILINE | re.IGNORECASE)
 		else:
-			p = re.compile(ur'^\w+ \d+ \d+:\d+:\d+ (<[^>]+>\t.*)', re.MULTILINE)
+			p = re.compile(ur'^\w+ \d+ \d+:\d+:\d+ (<(?!madbot|flanfly|SuikaIbuki|Wobbuffet|Internets|kindbot|ChanStat)[^>]+>\t[^.!~,].*)', re.MULTILINE)
 
 		try:
 
