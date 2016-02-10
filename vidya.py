b5e0573e7b5fd3741fe8815d2c3c6a3f5bcd7fd0
madprops <manuelchaves@gmail.com>
2016-02-10 15:53:38 -0600

    'tmfv'

--- vidya.py
+++ vidya.py
@@ -4,47 +4,82 @@ from willie.module import commands
 @commands('vidya')
 def vidya(bot, trigger, found_match=None):
 
-	arg = trigger[7:]
-
-	bot.say('searching for ' + arg)
+	global result_list
 
 	try:
+		result_list
+	except NameError:
+		result_list = []
 
-		gb = giantbomb.Api('c3cd72f9e232132125e9458294f296f73f720c91')
+	gb = giantbomb.Api('c3cd72f9e232132125e9458294f296f73f720c91')
 
-		results = gb.search(arg)
+	def get_game(id):
 
-		game = gb.getGame(results[0])
+		try:
 
-		s = u'\x02' + game.name + u'\x02' + " ("
+			game = gb.getGame(id)
 
-		for g in game.genres:
-		    s += g['name'] + ", "
+			s = u'\x02' + game.name + u'\x02' + " ("
 
-		s = s[:-2]
+			for g in game.genres:
+			    s += g['name'] + ", "
 
-		s += ") " + u'\x02' + "Released On: " + u'\x02' + game.original_release_date[:-9]
+			s = s[:-2]
 
-		s += u'\x02' + " Developed by: " + u'\x02'
+			s += ") " + u'\x02' + "Released On: " + u'\x02' + game.original_release_date[:-9]
 
-		for d in game.developers:
-		    s += d['name'] + ", "
+			s += u'\x02' + " Developed by: " + u'\x02'
 
-		s = s[:-2]
+			for d in game.developers:
+			    s += d['name'] + ", "
+
+			s = s[:-2]
 
 
-		s += u'\x02' + " Published by: " + u'\x02'
+			s += u'\x02' + " Published by: " + u'\x02'
 
-		for p in game.publishers:
-		    s += p['name'] + ", "
+			for p in game.publishers:
+			    s += p['name'] + ", "
 
-		s = s[:-2]
+			s = s[:-2]
 
-		print game.image
+			print game.image
 
-		s += u'\x02' + " URL: " + u'\x02' + game.site_detail_url
+			s += u'\x02' + " URL: " + u'\x02' + game.site_detail_url
 
-		bot.say(s)
+			bot.say(s)
 
-	except:
-		bot.say('game not found')
+		except:
+			bot.say('game not found')
+
+	arg = trigger[7:]
+
+	if arg.isdigit():
+		if len(result_list) == 0:
+			return
+		if int(arg) < 1 or int(arg) > len(result_list):
+			return
+		get_game(result_list[int(arg) - 1])
+
+	else:
+
+		bot.say('searching for ' + arg)
+
+		try:
+			results = gb.search(arg)[:8]
+			if len(results) == 0:
+				bot.say('game not found')
+			if len(results) == 1:
+				get_game(results[0])
+			else:
+				result_list = []
+				n = 1
+				s = ''
+				for g in results:
+					result_list.append(g.id)
+					s += u'\x02' + '(' + str(n) + ')' + u'\x02' + ' ' + g.name + ' '
+					n += 1
+				s = s[:-1]
+				bot.say("pick a game with '.vidya number' " + s)
+		except:
+			bot.say('game not found')
