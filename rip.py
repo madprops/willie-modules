23b552f859826d5d21b5e7e0aa8ca805a0b3a39f
madprops <manuelchaves@gmail.com>
2016-02-11 21:35:42 -0600

    'valuca'

--- rip.py
+++ rip.py
@@ -5,7 +5,7 @@ from willie.formatting import color
 
 @rule(ur"^rip\s.*$")
 def rip(bot, trigger, found_match=None):
-	arg = ' '.join(trigger[4:20].split()).strip()
+	arg = ' '.join(trigger[4:50].split()).strip()
 	if arg == '':
 		return
 	bot.say(color(u"✞" + ' ' + u"✞" + ' ' u"✞" + ' ' u"✞" + ' ' , '04', '00') + color(' ' + arg.upper() + ' ', '00', '04') + color(' ' + u"✞" + ' ' + u"✞" + ' ' u"✞" + ' ' u"✞", '04', '00'))
