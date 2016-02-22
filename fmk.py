90770b46fa1b82672ac32ad08193bb2a0d07df96
madprops <manuelchaves@gmail.com>
2016-02-22 17:06:24 -0600

    'vale'

--- fmk.py
+++ fmk.py
@@ -7,11 +7,11 @@ def fmk(bot, trigger, found_match=None):
 
 	nicks = trigger[5:].split(',')
 
-	if len(nicks) == 0:
+	if trigger[5:].strip() == '':
 		nicks = bot.privileges[Identifier(trigger.sender)].keys()
 	else:
 		if len(nicks) != 3:
-			bot.say('you must pick 3 persons')
+			bot.say('you must pick 3 things separated by commas')
 			return
 
 	random.shuffle(nicks)
