1e8fc34c8088fdb5fd73a76e28cac5023ea31b31
madprops <manuelchaves@gmail.com>
2016-04-03 12:28:58 -0600

    'nightcore'

--- tunnel.py
+++ tunnel.py
@@ -1,6 +1,15 @@
 # coding: utf-8
 
+import random
+
 from willie.module import commands
+
+def fill():
+	s = ''
+	n = random.randint(2, 7)
+	for x in range(1, n):
+		s += '.'
+	return s
 
 @commands('tunnel')
 def tunnel(bot, trigger, found_match=None):
@@ -9,8 +18,8 @@ def tunnel(bot, trigger, found_match=None):
 
 	if trigger.sender == '#podricing':
 		bot.msg('#neritic-net', msg, 1)
+		bot.say('sending message' + fill())
 
 	elif trigger.sender == '#neritic-net':
 		bot.msg('#podricing', msg, 1)
-
-	bot.say('message sent.')
+		bot.say('sending message' + fill())
