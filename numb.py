81b741751789ea29eab928cfffddfa05962c5255
madprops <manuelchaves@gmail.com>
2016-06-07 21:23:54 -0600

    mummb

--- numb.py
+++ numb.py
@@ -3,14 +3,21 @@ import string
 from willie.module import commands
 
 def string_to_num(txt):
-    characters = '0123456789' + string.ascii_letters
+    characters = string.ascii_letters
+    numbers = '0123456789'
     txt = txt.lower().strip()
     txt = ' '.join(txt.split())
     word_sums = 0
     for word in txt.split():
         word_sum = 0
         for letter in word:
-            n = characters.index(letter)
+            try:
+                n = characters.index(letter) + 1
+            except:
+                try:
+                    n = numbers.index(letter)
+                except:
+                    pass
             word_sum += n
         word_sums += word_sum
     return word_sums
@@ -23,6 +30,7 @@ def deconstruct(num, s=""):
     if len(str(sum)) > 1:
         return deconstruct(sum, s)
     else:
+        if s 
         return s
 
 @commands('numb')
--- trump.py
+++ trump.py
@@ -1,22 +1,10 @@
 # coding: utf-8
 
-from willie.module import rule
+from willie.module import commands
 from willie.formatting import color
-import datetime
 
-@rule(r'.*\bgreat\b.*')
+@commands('great')
 def trump(bot, trigger, found_match=None):
-
-	global timer
-
-	try:
-		timer
-	except NameError:
-		timer = datetime.datetime.now()
-
-	if datetime.datetime.now() - timer >= datetime.timedelta(minutes=30):
-
-		msg = '  MAKE AMERICA GREAT AGAIN!  T R U M P   2 0 1 6  '
-		msg = u'\x02' + color(msg, '04', '00') + u'\x02'
-		bot.say(msg)
-		timer = datetime.datetime.now()
+	msg = '  MAKE AMERICA GREAT AGAIN!  T R U M P   2 0 1 6  '
+	msg = u'\x02' + color(msg, '04', '00') + u'\x02'
+	bot.say(msg)
